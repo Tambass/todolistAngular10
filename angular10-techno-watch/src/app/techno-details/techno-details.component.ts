298f72704c7cc39605703d9b7f24913f811297ce
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-03-10 16:48:52 +0100

    ajoute un composant pour voir le détails dune techno

---
+++ angular10-techno-watch/src/app/techno-details/techno-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-techno-details',
+  templateUrl: './techno-details.component.html',
+  styleUrls: ['./techno-details.component.css']
+})
+export class TechnoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
